f282fa3ea4e2d53d388865a770d92f18d35db295
Noah-Smith-wgp <18211672297@163.com>
2019-11-20 23:35:06 +0800

    图形验证码后端逻辑

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,9 +1,17 @@
+from django import http
 from django.shortcuts import render
 from django.views import View
+from django_redis import get_redis_connection
 
+from verifications.libs.captcha.captcha import captcha
+from verifications import constants
 # Create your views here.
 
 
 class ImageCodeView(View):
     def get(self, request, uuid):
-        pass
+        text, image = captcha.generate_captcha()
+        redis_conn = get_redis_connection('verify_code')
+        redis_conn.setex('img_%s' % uuid, constants.IMAGE_CODE_REDIS_EXPIRES, text)
+
+        return http.HttpResponse(image, content_type='image/jpeg')
