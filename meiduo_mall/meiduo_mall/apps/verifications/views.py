af74ac3c2e07b316cf76d5fd604efd7b804a1c7e
Noah-Smith-wgp <18211672297@163.com>
2019-11-21 17:09:12 +0800

    短信验证码后端逻辑实现

--- meiduo_mall/meiduo_mall/apps/verifications/views.py
+++ meiduo_mall/meiduo_mall/apps/verifications/views.py
@@ -1,3 +1,5 @@
+import random, logging
+
 from django import http
 from django.shortcuts import render
 from django.views import View
@@ -5,7 +7,10 @@ from django_redis import get_redis_connection
 
 from verifications.libs.captcha.captcha import captcha
 from verifications import constants
+from meiduo_mall.utils.response_code import RETCODE
+from verifications.libs.yuntongxun.ccp_sms import CCP
 # Create your views here.
+logger = logging.getLogger('django')
 
 
 class ImageCodeView(View):
@@ -15,3 +20,35 @@ class ImageCodeView(View):
         redis_conn.setex('img_%s' % uuid, constants.IMAGE_CODE_REDIS_EXPIRES, text)
 
         return http.HttpResponse(image, content_type='image/jpeg')
+
+
+class SMSCodeView(View):
+    def get(self, request, mobile):
+        image_code_client = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+
+        if not all([image_code_client, uuid]):
+            return http.HttpResponseForbidden('缺少必传参数')
+
+        redis_conn = get_redis_connection('verify_code')
+        image_code_server = redis_conn.get('img_%s' % uuid)
+        if image_code_server is None:
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图形验证码失效'})
+
+        redis_conn.delete('img_%s' % uuid)
+
+        image_code_server = image_code_server.decode()
+        if image_code_client.lower() != image_code_server.lower():
+            return http.JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg':'图形验证码有误'})
+
+        #生成短信验证码
+        sms_code = '%06d' % random.randint(0, 999999)
+        logger.info(sms_code)
+
+        redis_conn.setex('sms_%s' % mobile, constants.SMS_CODE_REDIS_EXPIRES, sms_code)
+
+        CCP().send_template_sms(mobile, [sms_code, constants.SMS_CODE_REDIS_EXPIRES //60], constants.SEND_SMS_TEMPLATE_ID)
+
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '发送短信成功'})
+
+
