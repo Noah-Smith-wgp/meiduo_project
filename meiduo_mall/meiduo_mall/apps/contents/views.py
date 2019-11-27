8a68b9daa86faf92882fe3072c372394b824460a
Noah-Smith-wgp <18211672297@163.com>
2019-11-27 14:43:25 +0800

    展示首页商品广告

--- meiduo_mall/meiduo_mall/apps/contents/views.py
+++ meiduo_mall/meiduo_mall/apps/contents/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from django.views import View
 from contents.utils import get_categories
-from contents.models import GoodsChannel
+from contents.models import ContentCategory
 # Create your views here.
 
 
@@ -12,8 +12,14 @@ class IndexView(View):
         """提供首页广告页面"""
         # 查询商品分类
         categories = get_categories()
+
+        contents = {}
+        content_categories = ContentCategory.objects.all()
+        for cat in content_categories:
+            contents[cat.key] = cat.content_set.filter(status=True).order_by('sequence')
         # 构造上下文
         context = {
-            'categories': categories
+            'categories': categories,
+            'contents': contents
         }
         return render(request, 'index.html', context)
