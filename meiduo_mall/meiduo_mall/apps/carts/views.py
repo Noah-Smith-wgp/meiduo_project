cb4c81232f1c4ee256e1888c57750e7e723d8169
Noah-Smith-wgp <18211672297@163.com>
2019-12-01 21:52:09 +0800

    修改购物车-登录状态

--- meiduo_mall/meiduo_mall/apps/carts/views.py
+++ meiduo_mall/meiduo_mall/apps/carts/views.py
@@ -119,7 +119,48 @@ class CartsView(View):
 
     def put(self, request):
         """修改购物车"""
-        pass
+        #接收参数
+        json_dict = json.loads(request.body.decode())
+        sku_id = json_dict.get('sku_id')
+        count = json_dict.get('count')
+        selected = json_dict.get('selected', True)
+
+        #校验参数
+        if not all([sku_id, count]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        try:
+            sku = SKU.objects.get(id=sku_id)
+        except SKU.DoesNotExist:
+            return http.HttpResponseForbidden('参数sku_id错误')
+        try:
+            count = int(count)
+        except Exception:
+            return http.HttpResponseForbidden('参数count错误')
+        if not isinstance(selected, bool):
+            return http.HttpResponseForbidden('参数selected错误')
+
+        #判断用户是否登录
+        user = request.user
+        if user.is_authenticated:
+            redis_conn = get_redis_connection('carts')
+            redis_conn.hset('carts_%s' % user.id, sku_id, count)
+            if selected:
+                redis_conn.sadd('selected_%s' % user.id, sku_id)
+            else:
+                redis_conn.srem('selected_%s' % user.id, sku_id)
+
+            cart_sku = {
+                'id': sku_id,
+                'name': sku.name,
+                'price': sku.price,
+                'default_image_url': sku.default_image.url,
+                'count': count,
+                'selected': selected,
+                'amount': sku.price * count
+            }
+            return http.JsonResponse({'code': RETCODE.OK, 'errmsg': '修改购物车成功', 'cart_sku':cart_sku})
+        else:
+            pass
 
     def delete(self, request):
         """删除购物车"""
