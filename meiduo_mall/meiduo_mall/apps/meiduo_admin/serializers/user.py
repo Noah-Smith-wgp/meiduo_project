11054cc61342cef993cd33c28a7e67f392f3845b
Noah-Smith-wgp <18211672297@163.com>
2019-12-15 22:42:17 +0800

    确认密码的实现

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/user.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/user.py
@@ -5,9 +5,11 @@ from users.models import User
 
 class UserListViewSerializer(serializers.ModelSerializer):
 
+    password2 = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['id', 'username', 'mobile', 'email', 'password']
+        fields = ['id', 'username', 'mobile', 'email', 'password', 'password2']
 
         extra_kwargs = {
             'password': {
@@ -15,7 +17,17 @@ class UserListViewSerializer(serializers.ModelSerializer):
             }
         }
 
+    def validate(self, attrs):
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        if password != password2:
+            raise serializers.ValidationError('密码不一致')
+
+        return attrs
+
     def create(self, validated_data):
+
+        del validated_data['password2']
         user = User.objects.create_user(**validated_data)
 
         return user
