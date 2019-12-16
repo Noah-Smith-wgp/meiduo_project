82da580bde8176df6f8e3178703871d355cb6b1a
Noah-Smith-wgp <18211672297@163.com>
2019-12-16 21:57:14 +0800

    新增sku表数据之保存sku数据

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/sku.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/sku.py
@@ -1,14 +1,42 @@
 from rest_framework import serializers
 
 from contents.models import GoodsCategory
-from goods.models import SKU, SPU, SPUSpecification, SpecificationOption
+from goods.models import SKU, SPU, SPUSpecification, SpecificationOption, SKUSpecification
+
+
+# class SKUSpecificationSerializer(serializers.ModelSerializer):
+#
+#     spec_id = serializers.IntegerField()
+#     option_id = serializers.IntegerField()
+#
+#     class Meta:
+#         model = SKUSpecification
+#         fields = ['spec_id', 'option_id']
 
 
 class SKUSerializer(serializers.ModelSerializer):
 
+    spu = serializers.StringRelatedField(read_only=True)
+    category = serializers.StringRelatedField(read_only=True)
+
+    spu_id = serializers.IntegerField()
+    category_id = serializers.IntegerField()
+
+    # specs = SKUSpecificationSerializer(many=True)
+
     class Meta:
         model = SKU
         fields = '__all__'
+
+    def create(self, validated_data):
+        specs = self.context['request'].data.get('specs')
+
+        sku = SKU.objects.create(**validated_data)
+
+        for item in specs:
+            SKUSpecification.objects.create(sku=sku, spec_id=item.get('spec_id'), option_id=item.get('option_id'))
+
+        return sku
 
 
 class SKUCategorieSerializer(serializers.ModelSerializer):
