a743d3dd99cc9710943f99ca13821ce795b3d712
Noah-Smith-wgp <18211672297@163.com>
2019-12-17 21:17:44 +0800

    保存图片的第二种方法

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/image.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/serializers/image.py
@@ -6,11 +6,35 @@ from goods.models import SKUImage, SKU
 class ImageSerializer(serializers.ModelSerializer):
 
     # 返回图片关联的sku的id
-    sku = serializers.PrimaryKeyRelatedField(read_only=True)
+    # sku = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = SKUImage
         fields = ['id', 'sku', 'image']
+
+    def create(self, validated_data):
+        sku = validated_data.get('sku')
+        image = validated_data.get('image')
+
+        from fdfs_client.client import Fdfs_client
+        # 创建fastdfs连接对象
+        client = Fdfs_client('meiduo_mall/utils/fastdfs/client.conf')
+
+        result = client.upload_by_buffer(image.read())
+
+        if result['Status'] == 'Upload successed.':
+            # 获取上传后的路径
+            image_url = result.get('Remote file_id')
+
+            # windows系统需要用此方法修改image_url,将group1\\M00/00/02/...改为group1/M00/00/02/...
+            image_url = image_url.replace('\\', '/')
+
+            # 保存图片
+            img = SKUImage.objects.create(sku=sku, image=image_url)
+
+            return img
+        else:
+            raise serializers.ValidationError('上传失败')
 
 
 class SKUSerializer(serializers.ModelSerializer):
--- meiduo_mall/meiduo_mall/apps/meiduo_admin/views/image.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/views/image.py
@@ -17,34 +17,34 @@ class ImageViewSet(ModelViewSet):
     # 分页  # GenericAPIView 及其子类才有分页
     pagination_class = PageNum
 
-    def create(self, request, *args, **kwargs):
-
-        sku_id = request.data.get('sku')
-        image = request.FILES.get('image')
-
-        from fdfs_client.client import Fdfs_client
-        # 创建fastdfs连接对象
-        client = Fdfs_client('meiduo_mall/utils/fastdfs/client.conf')
-
-        result = client.upload_by_buffer(image.read())
-
-        if result['Status'] == 'Upload successed.':
-            # 获取上传后的路径
-            image_url = result.get('Remote file_id')
-
-            # windows系统需要用此方法修改image_url,将group1\\M00/00/02/...改为group1/M00/00/02/...
-            image_url = image_url.replace('\\', '/')
-
-            # 保存图片
-            img = SKUImage.objects.create(sku_id=sku_id, image=image_url)
-
-            return Response({
-                'id': img.id,
-                'sku_id': sku_id,
-                'image': img.image.url
-            }, status=status.HTTP_201_CREATED)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+    # def create(self, request, *args, **kwargs):
+    #
+    #     sku_id = request.data.get('sku')
+    #     image = request.FILES.get('image')
+    #
+    #     from fdfs_client.client import Fdfs_client
+    #     # 创建fastdfs连接对象
+    #     client = Fdfs_client('meiduo_mall/utils/fastdfs/client.conf')
+    #
+    #     result = client.upload_by_buffer(image.read())
+    #
+    #     if result['Status'] == 'Upload successed.':
+    #         # 获取上传后的路径
+    #         image_url = result.get('Remote file_id')
+    #
+    #         # windows系统需要用此方法修改image_url,将group1\\M00/00/02/...改为group1/M00/00/02/...
+    #         image_url = image_url.replace('\\', '/')
+    #
+    #         # 保存图片
+    #         img = SKUImage.objects.create(sku_id=sku_id, image=image_url)
+    #
+    #         return Response({
+    #             'id': img.id,
+    #             'sku_id': sku_id,
+    #             'image': img.image.url
+    #         }, status=status.HTTP_201_CREATED)
+    #     else:
+    #         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class SKUListView(ListAPIView):
